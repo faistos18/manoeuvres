fad97c162b1f2bdb3016dbef485ac5450742b510
mathieu_galle <mathieu.galle@bitbucket.org>
2020-04-13 15:47:32 +0200

    terminé le C++ avec la fonctio aller directement

--- main.cpp
+++ main.cpp
@@ -315,9 +315,6 @@ void transformeButVersRefBraquage(const PointAngle &pointAngle, const PointAngle
     // double butPointAngleAngle = butPointAngle.angleRad - pointAngle.angleRad;
 
     rotation(xOut, yOut, 0, 0, -pointAngle.angleRad, xOut, yOut);
-    // symétrie gauche droite et symétrie avant arriere =>
-    xOut = abs(xOut);
-    yOut = abs(yOut);
 }
 
 void calculMouvementsInaccessible()
@@ -329,6 +326,11 @@ double heuristiqueMeilleureDistanceInaccessible(const PointAngle &pointAngle, co
     double butX, butY, distanceTotale;
 
     transformeButVersRefBraquage(pointAngle, butPointAngle, rayonBraquage, butX, butY);
+    // symétrie avant / arriere
+    butX = abs(butX);
+    //  gauche / droite
+    butY = abs(butY);
+
     bool inaccessible = (butX * butX + (butY - 1.0) * (butY - 1.0)) < 1.0; //dans le cercle rayon de braquage
     if (inaccessible)
     {
@@ -347,7 +349,7 @@ double heuristiqueMeilleureDistanceInaccessible(const PointAngle &pointAngle, co
         // or b = 2, a = 1, c = l
         // on obtient gamma1 = acos( (l²+3)/4l)
         double gamma1 = acos((l * l + 3.0) / (4.0 * l));
-        double dist1 = gamma1 - angleL;
+        double dist1 = gamma1 + angleL; // normalement : - angleL. test avec +.
         //  on réapplique Al kashi avec l'autre angle
         //  a=1, b = 2, c=l, on obtient
         //  acos( (5-l²) / 4)
@@ -369,6 +371,122 @@ double heuristiqueMeilleureDistanceInaccessible(const PointAngle &pointAngle, co
     return max(heuristiqueAngle(pointAngle, butPointAngle, rayonBraquage) - toleranceAngle, distanceTotale);
 }
 
+// LA FONCTION POUR XAVIER
+
+void chercherMeilleureManoeuvreSansAngle(PointAngle &pointAngle, const PointAngle &butPointAngle, bool prefererArriere, double rayonBraquage, MOUVEMENT *mouvement1, double *outDist1, MOUVEMENT *mouvement2, double *outDist2, double *outAngleArrivee)
+{
+    double butX, butY;
+
+    transformeButVersRefBraquage(pointAngle, butPointAngle, rayonBraquage, butX, butY);
+    // symétrie gauche / droite avant / arriere
+    bool inaccessible = (butX * butX + (abs(butY) - 1.0) * (abs(butY) - 1.0)) < 1.0; //dans le cercle rayon de braquage
+    bool symetrieX = butX < 0, symetrieY = butY < 0;
+    if (inaccessible)
+    {
+        if (symetrieY)
+        {
+            if (symetrieX && !prefererArriere)
+            {
+                *mouvement1 = AVANT_GAUCHE;
+                *mouvement2 = ARRIERE_DROIT;
+            }
+            else
+            {
+                *mouvement1 = ARRIERE_GAUCHE;
+                *mouvement2 = AVANT_DROIT;
+            }
+        }
+        else
+        {
+            if (symetrieX && !prefererArriere)
+            {
+                *mouvement1 = AVANT_DROIT;
+                *mouvement2 = ARRIERE_GAUCHE;
+            }
+            else
+            {
+                *mouvement1 = ARRIERE_DROIT;
+                *mouvement2 = AVANT_GAUCHE;
+            }
+        }
+    }
+    else
+    {
+        if (symetrieX)
+        {
+            if (symetrieY)
+                *mouvement1 = ARRIERE_DROIT;
+            else
+                *mouvement1 = ARRIERE_GAUCHE;
+            *mouvement2 = RECULE;
+        }
+        else
+        {
+            if (symetrieY)
+                *mouvement1 = AVANT_DROIT;
+            else
+                *mouvement1 = AVANT_GAUCHE;
+            *mouvement2 = AVANCE;
+        }
+    }
+    butY = abs(butY);
+    if (!inaccessible || !prefererArriere)
+    {
+        butX = abs(butX);
+    }
+
+    if (inaccessible)
+    {
+        //symétrie gauche / droite
+        butY = abs(butY);
+
+        // dans le cercle, on peut calculer les manoeuvres.
+        // addition d'une manoeuvre où on recule, une où on avance. dist1, dist2
+        // on utilise forcément le côté "vert", donc une rotation
+        // C => centre du cercle du bas, (0, -1)
+        // l = vecteur C => But
+        double lY = butY + 1.0;
+        double l = sqrt(butX * butX + lY * lY);
+
+        double angleL = atan2(lY, butX) - M_PI / 2.0;
+        // because reference is y axis, not x axis
+        // al kashi : c² = b² + a² - 2ab*gamma (gamma1 = angle F-C-But)
+        // => gamma1 = acos( (a²+b²-c²)/2ab)
+        // or b = 2, a = 1, c = l
+        // on obtient gamma1 = acos( (l²+3)/4l)
+        double gamma1 = acos((l * l + 3.0) / (4.0 * l));
+        //  on réapplique Al kashi avec l'autre angle
+        //  a=1, b = 2, c=l, on obtient
+        //  acos( (5-l²) / 4)
+        double gamma2 = acos((5.0 - l * l) / 4.0);
+        *outDist1 = gamma1 + angleL;
+        *outDist2 = gamma2;
+        *outAngleArrivee = gamma1 + gamma2 + angleL;
+    }
+    else // accessible
+    {
+        double longueurL = sqrt(butX * butX + (butY - 1.0) * (butY - 1.0));
+        double longueurSegment = sqrt(longueurL * longueurL - 1.0);
+
+        double angleL = atan2(butY - 1.0, butX);
+        double angleTriangle = acos(1.0 / longueurL);
+        double angle = angleL - angleTriangle + (M_PI / 2.0);
+
+        *outDist1 = angle;
+        *outDist2 = longueurSegment;
+        *outAngleArrivee = angle;
+    }
+    // correction scaling
+    *outDist1 *= rayonBraquage;
+    *outDist2 *= rayonBraquage;
+    //symetries
+    if (symetrieY)
+        *outAngleArrivee = -(*outAngleArrivee);
+
+    if (symetrieX && !(inaccessible && prefererArriere))
+        *outAngleArrivee = -(*outAngleArrivee);
+}
+
 PointAngle *prendreMeilleurCandidat(list<PointAngle *> &pointsAChercher, const PointAngle &butPointAngle, double rayonBraquage, double toleranceAngle)
 {
     double meilleureEstimation = 100000000, estimation = 0;
@@ -384,7 +502,8 @@ PointAngle *prendreMeilleurCandidat(list<PointAngle *> &pointsAChercher, const P
         else
         {
             estimation = (*pointIterator)->distanceParcourue + heuristiqueMeilleureDistanceInaccessible(**pointIterator, butPointAngle, rayonBraquage, toleranceAngle);
-            if( (*pointIterator)->pere != NULL) {
+            if ((*pointIterator)->pere != NULL)
+            {
                 estimation = max(estimation, (*pointIterator)->pere->estimation);
             }
             (*pointIterator)->estimation = estimation;
@@ -498,12 +617,14 @@ bool chercherMeilleureManoeuvre(
     return false;
 }
 
+//Fonctions de test
+
 void testRotation()
 {
     double xr = 0;
     double yr = 0;
     rotation(0, 0, 0, 1, M_PI, xr, yr);
-    cout << "hello gateau : " << xr << ", " << yr << "  ok" << endl;
+    cout << "testRotation : " << xr << ", " << yr << "  ok" << endl;
 }
 
 void testHeuristiqueInaccessible()
@@ -520,32 +641,103 @@ void testHeuristiqueInaccessible()
     cout << "quand l = 1 " << test3 << endl;
 }
 
-/*
-Améliorations possibles :
-puisque j'utilise des pointeurs de toutes façons, je peux mettre un pointeur vers pere dans PointAngle,
-et du coup virer la structure compliquée pour std::vector<tuple> pour stocker les mouvements.
-Je peux faire l'optimiser où l'heuristique augmente jamais.
-Je peux implémenter la fonction heuristique inaccessible, comme c'est fait en python 
-=> environ facteur 4 sur les cas compliqués.
-Ou je peux utiliser un pas de 0.3 ou 0.25 au lieu de 0.2...
+void testMeilleureManoeuvreSansAngleXYPreferer(double x, double y, bool prefererArriere)
+{
+    cout << "point a tester : " << x << ", " << y << (prefererArriere ? " arriere" : "") << endl;
 
-Sinon, fonction de xavier pour les cas compliqués ?
-Je rajoute un paramètres avant, bool, et la fonction renvoie le truc ?
-*/
+    double rayonBraquage = 1.90;
+    PointAngle depart = PointAngle(0, 0, 0);
+    PointAngle arrivee = PointAngle(x, y, 0);
+    double distance1, distance2, angleArrivee;
+    MOUVEMENT mouvement1, mouvement2;
+    chercherMeilleureManoeuvreSansAngle(depart, arrivee, prefererArriere, rayonBraquage, &mouvement1, &distance1, &mouvement2, &distance2, &angleArrivee);
+
+    //Application du mouvement
+    PointAngle *apresMouvement1 = depart.bouge(mouvement1, distance1, rayonBraquage);
+    PointAngle *apresMouvement2 = apresMouvement1->bouge(mouvement2, distance2, rayonBraquage);
+    double epsilon = 0.000001;
+
+    if (abs(apresMouvement2->x - x) < epsilon && abs(apresMouvement2->y - y) < epsilon)
+    {
+        cout << "ok position" << endl;
+    }
+    else
+    {
+        cout << "FAIL position" << endl;
+        cout << apresMouvement2->x << " vs. " << x << endl;
+        cout << apresMouvement2->y << " vs. " << y << endl;
+    }
+
+    if (abs(apresMouvement2->angleRad - angleArrivee) < epsilon)
+    {
+        cout << "ok angle" << endl;
+    }
+
+    delete apresMouvement1;
+    delete apresMouvement2;
+}
+void testMeilleureManoeuvreSansAngle()
+{
+    double tests[8][2] = {
+        //accessibles
+        {4, 1},
+        {-4, 1},
+        {4, -1},
+        {-4, -1},
+        //inaccessibles
+        {0.1, 0.5},
+        {-0.1, 0.5},
+        {0.1, -0.5},
+        {-0.1, -0.5}};
+    for (unsigned int i = 0; i < 8; i++)
+    {
+        testMeilleureManoeuvreSansAngleXYPreferer(tests[i][0], tests[i][1], true);
+        testMeilleureManoeuvreSansAngleXYPreferer(tests[i][0], tests[i][1], false);
+    }
+}
+
 int main()
 {
     //testRotation();
     //testMouvements();
     //testHeuristiqueInaccessible();
 
-    
-    PointAngle but(0.0, 1.7, 0.0);
+    //testMeilleureManoeuvreSansAngle();
 
-    chercherMeilleureManoeuvre(but);
+    cout << " 1 : calculer manoeuvre avec angle (algo de recherche), 2 : calculer manoeuvre sans angle" << endl;
+    int l = 2;
+    cin >> l;
+    cout << (l == 1 ? "manoeuvre avec angle (algo de recherche) !" : "manoeuvre sans angle" ) << endl;
 
-    // */
-    double wait = 0.f;
-    cin >> wait;
+    double x, y;
+    cout << " x ? " << endl;
+    cin >> x;
+    cout << " y ? " << endl;
+    cin >> y;
+
+    if (l == 1)
+    {
+        double angle;
+        cout << "angle en deg ? " << endl;
+        cin >> angle;
+        angle = angle * M_PI / 180.0;
+
+        cout << "calcul en cours..." << endl;
+        chercherMeilleureManoeuvre(PointAngle(x, y, angle));
+    }
+    else
+    {
+        bool prefererArriere = true;
+        cout << " preferer en arriere ? 0/1" << endl;
+        cin >> prefererArriere;
+        double distance1, distance2, angleArrivee;
+        MOUVEMENT mouvement1, mouvement2;
+        PointAngle depart(0, 0, 0);
+        chercherMeilleureManoeuvreSansAngle(depart, PointAngle(x, y, 0), prefererArriere, 1.90, &mouvement1, &distance1, &mouvement2, &distance2, &angleArrivee);
+        cout << "mouvement 1 : " << MOUVEMENTS_TEXTE[mouvement1] << " avec distance " << distance1 << endl;
+        cout << "mouvement 2 : " << MOUVEMENTS_TEXTE[mouvement2] << " avec distance " << distance2 << endl;
+        cout << "arrivee avec un angle de " << (angleArrivee * 180.0 / M_PI) << " deg" <<endl;
+    }
 
     return 0;
 }
